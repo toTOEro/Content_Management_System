8074aa373213b9b866388501285988db5777bb5a
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-01 18:41:40 -0700

    Further troubleshooting of returning departments from query

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const DbPing = require('./lib/queries')
 const cTable = require('console.table');
-const { userAction, addDepartment, newEmployee, newRole, updateRole } = require('./lib/questions')
+// const { userAction, addDepartment, newEmployee, newRole, updateRole } = require('./lib/questions')
+
 
 // DB Initialization
 
@@ -19,6 +20,17 @@ const db = new DbPing(mysql.createConnection(
 ))
 
 
+// const db = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'password',
+//         database: 'employee_db'
+//     },
+//     console.log(`Connected to the employee_db database.`)
+// );
+
+// db.getDepartments().then((res) => console.log(res))
 
 // This function initializes the app to begin asking questions.
 function init() {
@@ -28,8 +40,8 @@ function init() {
             switch (response.userAction) {
                 case 'View all departments':
                     db.viewDepartment()
+                        .then((res) => console.log(res))
                         .then(() => init());
-                    // init();
                     return;
 
                 case 'View all roles':
@@ -50,15 +62,21 @@ function init() {
                     return;
 
                 case 'Add a role':
+                    let departments = [];
+                    
+
+
+                    
+
                     inquirer.prompt(newRole)
                         .then(({ roleName, salary, department }) => {
-                            
+
                         })
 
 
-                    db.addRole()
-                        .then(() => init());
-                    return;
+                // // db.addRole()
+                // //     .then(() => init());
+                // return;
 
                 case 'Add an employee':
                     db.addEmployee()
@@ -81,6 +99,122 @@ function init() {
         })
 
 }
+
+
+
+
+
+
+// Inquirer Questions 
+const userAction = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'userAction',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Quit'
+        ]
+    },
+
+];
+
+const addDepartment = [
+    {
+        type: 'input',
+        message: 'What is the department name?',
+        name: 'departmentName',
+    }
+];
+
+const newEmployee = [
+    {
+        type: 'input',
+        message: 'What is the first name?',
+        name: 'employeeFirstName',
+    },
+    {
+        type: 'input',
+        message: 'What is the first name?',
+        name: 'employeeLastName',
+    },
+    {
+        type: 'input',
+        message: 'What department?',
+        name: 'employeeDepartment',
+    },
+    {
+        type: 'input',
+        message: 'What is their role?',
+        name: 'employeeRole',
+    },
+    {
+        type: 'input',
+        message: 'Who is their manager?',
+        name: 'employeeManager'
+    }
+];
+
+const newRole = [
+    {
+        type: 'input',
+        message: 'What is the role name?',
+        name: 'roleName',
+    },
+    {
+        type: 'number',
+        message: 'What is the salary?',
+        name: 'roleSalary',
+    },
+    {
+        type: 'list',
+        message: 'What department is this role?',
+        name: 'roleDepartment',
+        choices: deptNames
+    }
+
+]
+
+const updateRole = [
+    {
+        type: 'input',
+        message: 'What is their new role?',
+        name: 'newRole',
+    },
+
+]
+
+console.log(newRole)
+
+function deptChoices() {
+    db.db.promise().query(`SELECT * FROM department
+    ORDER BY id;`).then(([rows, fields]) => {
+        departments = rows.map(({name}) => name)
+        return departments
+    });
+}
+
+// async function getD() {
+//     // db.getDepartments();
+//     return ['1', '2']
+// }
+
+// console.log(getD())
+// console.log(db.getDepartments())
+
+// let test = db.getDepartments();
+// console.log(test)
+// test.then((res) => console.log(res))
+
+
+
+
 
 // Function call to initialize app
 init();
